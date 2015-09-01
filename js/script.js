21746e30156add1d05ae60f3e0bed6bf626af42b
Pablo <pabloromanh@gmail.com>
2015-09-01 10:39:59 +0700

    Added search results with visualization and adding of comments.

--- js/script.js
+++ js/script.js
@@ -1,47 +1,77 @@
-$(document).ready(function() {
-    window.db = new Firebase('https://fitners.firebaseio.com/coaches/');
-    $("#searchform").submit(find);
-});
+(function() {
+    var app = angular.module('fitners', []);
 
-function find() {
-    var gym = document.getElementById('gym').value;
-
-    $("#results").empty();
-    $("#results").append("Buscando...");
-
-    window.db.orderByChild('gym').equalTo(gym).limitToFirst(10).once('value', function(snapshot) {
-        if(snapshot.val() === null) {
-            // TODO: show no results message
-            $("#results").empty();
-            $("#results").append("No se encontró nada");
-            return;
-        }
-
-        var results = snapshot.val();
-        console.log('results');
-        console.log(results);
-
-        // TODO: sort by number of stars
-
-        $("#results").empty();
-
-        for (var i = 0; i < results.length; i++) {
-            if (!results[i]) continue; // HACK: Query results sometimes include undefined elements. Investigate.
-
-            var commentslink = $('<a href="#">valoraciones</a>');
-
-            commentslink.click(function () {
-                console.log('clicked');
-            });
-
-            var newdiv = $("<div></div>")
-                .append(results[i].name)
-                .append($('<a href="tel:1-555-555-5555">1-555-555-5555</a>'))
-                .append(commentslink);
-            $("#results").append(newdiv);
-        }
-
+    app.config(function ($locationProvider) {
+        $locationProvider.html5Mode(true);
     });
 
-    return false;
-}
+    app.controller('SearchController', function($scope){
+        var controller = this;
+        controller.results = [];
+        controller.searching = false;
+        controller.noresult = false;
+        controller.gym = "";
+
+        var db = new Firebase('https://fitners.firebaseio.com/coaches/');
+
+        controller.search = function() {
+            controller.results = [];
+            controller.searching = true;
+            controller.noresult = false;
+
+            db.orderByChild('gym').equalTo(controller.gym).limitToFirst(10).once('value', function(snapshot) {
+                if(snapshot.val() === null) {
+                    controller.searching = false;
+                    controller.noresult = true;
+                    return;
+                }
+
+                controller.searching = false;
+
+                // TODO: sort by number of stars
+
+                snapshot.forEach(function(data) {
+                    var value = data.val();
+                    value.id = data.key();
+
+                    controller.results.push(value);
+                });
+
+                $scope.$apply();
+            });
+
+            return false;
+        }
+
+        controller.showComments = function(coach) {
+            controller.showModal = 'comments';
+            controller.selectedCoach = coach;
+        }
+
+        controller.submitComment = function() {
+            controller.showModal = false;
+            console.log('Submitting new comment', controller.comment, controller.stars);
+
+            db.child(controller.selectedCoach.id +'/ratings').push().set({
+                 comment: controller.comment,
+                 name: 'anonymous',
+                 stars: parseInt(controller.stars)
+            });
+
+            controller.comment = undefined;
+            controller.stars = undefined;
+        }
+    });
+
+    app.directive('ngModal', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                scope.$watch(attrs.ngModal, function(value) {
+                    if (value) element.modal('show');
+                    else element.modal('hide');
+                });
+            }
+        };
+    });
+})();
