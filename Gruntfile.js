5ad0a6ac2e2eebc83a086153ac2f41de30b0b92b
Pablo <pabloromanh@gmail.com>
2015-10-24 19:44:40 +0700

    Added Grunt to the project.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['js/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+        }
+        // uglify: {
+        //     options: {
+        //         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        //     },
+        //     build: {
+        //         src: 'src/<%= pkg.name %>.js',
+        //         dest: 'build/<%= pkg.name %>.min.js'
+        //     }
+        // }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jshint']);
+
+};
